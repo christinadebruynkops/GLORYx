dcce68ac6efc7a8ec4c32fb0699aac57b4ae9562
Martin Sicho <sicho@zbh.uni-hamburg.de>
2016-10-19 10:33:39 +0200

    fixed a problem with the header in the csv file and the SD file with reaction annotations was added

--- descriptors.sh
+++ descriptors.sh
@@ -5,7 +5,7 @@ set -e
 cd run_dir
 
 (
-  java -jar ../out/artifacts/descriptors/metasar.jar MetaSAR_all_annotated.sdf \
+  java -jar ../out/artifacts/descriptors/metasar.jar MetaSAR_all_annotated_rxns.sdf \
   | tee descriptors.log
 ) 3>&1 1>&2 2>&3 | tee descriptors_errors.log
 
@@ -14,4 +14,4 @@ PATTERN='*.csv'
 rm -f all_data.csv
 cat $PATTERN | egrep -v 'Mol' > all_data.csv
 FILES=( $PATTERN )
-echo -e "$(head -1 ${FILES[0]})$(cat all_data.csv)" > all_data.csv
+echo -e "$(head -1 ${FILES[0]})\n$(cat all_data.csv)" > all_data.csv
