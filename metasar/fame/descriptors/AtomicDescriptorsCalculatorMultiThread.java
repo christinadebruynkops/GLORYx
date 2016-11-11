1e1476926deebad94401ebf783ae455e0acd4af8
Martin Sicho <sicho@zbh.uni-hamburg.de>
2016-11-11 14:38:30 +0100

    turned sanitization on because we will need it after all

--- metasar/fame/descriptors/AtomicDescriptorsCalculatorMultiThread.java
+++ metasar/fame/descriptors/AtomicDescriptorsCalculatorMultiThread.java
@@ -30,7 +30,7 @@ public class AtomicDescriptorsCalculatorMultiThread {
 
 		// sanitize the data and get the path to the modified file
 		String input_file = args[0];
-//		input_file = Sanitize.sanitize(input_file);
+		input_file = Sanitize.sanitize(input_file);
 	    
 		@SuppressWarnings("rawtypes")
 		DefaultIteratingChemObjectReader reader = (IteratingMDLReader) new IteratingMDLReader(new FileInputStream(input_file), DefaultChemObjectBuilder.getInstance());
