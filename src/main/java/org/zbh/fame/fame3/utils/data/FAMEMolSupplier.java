1fa414ca5574ab736fe235ca89802905884554ba
martin-sicho <sicho.martin@gmail.com>
2019-08-14 20:31:54 +0200

    added error collection code to FAMEMolSupplier

--- src/main/java/org/zbh/fame/fame3/utils/data/FAMEMolSupplier.java
+++ src/main/java/org/zbh/fame/fame3/utils/data/FAMEMolSupplier.java
@@ -2,6 +2,7 @@ package org.zbh.fame.fame3.utils.data;
 
 import org.openscience.cdk.interfaces.IAtomContainer;
 import org.zbh.fame.fame3.utils.data.parsers.FAMEFileParser;
+import org.zbh.fame.fame3.utils.data.parsers.FAMEFileParserException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,14 +11,17 @@ public class FAMEMolSupplier {
 
     private List<FAMEFileParser> parsers;
     private FAMEFileParser current_parser;
+    private List<FAMEFileParserException> errors;
 
     public FAMEMolSupplier(FAMEFileParser parser) {
         parsers = new ArrayList<>();
+        errors = new ArrayList<>();
         current_parser = parser;
     }
 
     public FAMEMolSupplier(List<FAMEFileParser> parsers) {
         this.parsers = parsers;
+        this.errors = new ArrayList<>();
         this.current_parser = this.parsers.remove(parsers.size() - 1);
     }
 
@@ -38,11 +42,16 @@ public class FAMEMolSupplier {
             return true;
         } else if (!parsers.isEmpty()){
             while (!parsers.isEmpty() && !current_parser.hasNext()) {
+                errors.addAll(current_parser.getErrors());
                 current_parser = parsers.remove(parsers.size() - 1);
             }
             return current_parser.hasNext();
         } else {
             return false;
         }
+    }
+
+    public List<FAMEFileParserException> getErrors() {
+        return errors;
     }
 }
